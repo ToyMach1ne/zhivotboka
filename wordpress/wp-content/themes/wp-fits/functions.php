059d1c7169266bcf1a42f4dd5055c88dcc73a35a
Toy Mach1ne <forvinwolves@gmail.com>
2016-11-23 18:46:43 +0200

    widgets

--- wordpress/wp-content/themes/wp-fits/functions.php
+++ wordpress/wp-content/themes/wp-fits/functions.php
@@ -660,9 +660,70 @@ function disable_emojicons_tinymce( $plugins ) {
 }
 
 
+class RandomPostWidget extends WP_Widget
+{
+  function RandomPostWidget()
+  {
+    $widget_ops = array('classname' => 'widget-last-news', 'description' => 'Displays a random post with thumbnail' );
+    $this->WP_Widget('RandomPostWidget', 'Random Post and Thumbnail', $widget_ops);
+  }
 
+  function form($instance)
+  {
+    $instance = wp_parse_args( (array) $instance, array( 'title' => '' ) );
+    $title = $instance['title'];
+?>
+  <p><label for="<?php echo $this->get_field_id('title'); ?>">Title: <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo attribute_escape($title); ?>" /></label></p>
+  <?php }
 
+  function update($new_instance, $old_instance)
+  {
+    $instance = $old_instance;
+    $instance['title'] = $new_instance['title'];
+    return $instance;
+  }
 
+  function widget($args, $instance)
+  {
+    extract($args, EXTR_SKIP);
+
+    echo $before_widget;
+    $title = empty($instance['title']) ? ' ' : apply_filters('widget_title', $instance['title']);
+
+    if (!empty($title))
+      echo $before_title . $title . $after_title;;
+
+    // WIDGET CODE GOES HERE
+    query_posts("showposts=5");
+    if ( have_posts() ) {
+
+      while ( have_posts() ) {
+
+      the_post();
+
+      if ( has_post_thumbnail()) {
+        $image = the_post_thumbnail('small');
+      } else {
+        $image = '<img src="'. catchFirstImage() .'" title="'. get_the_title() .'" alt="'. get_the_title() .'" />';
+      }
+
+      echo sprintf('<div class="last-news">
+        <a href="%s">
+          <span class="category-block-img">
+          %s
+          </span>
+          %s
+        </a>
+      </div><!-- last-news -->', get_permalink(), $image, wpeExcerpt('wpeExcerpt6'));
+      }
+    }
+    wp_reset_query();
+
+    echo $after_widget;
+  }
+
+}
+add_action( 'widgets_init', create_function('', 'return register_widget("RandomPostWidget");') );
 
 
 
--- wordpress/wp-content/themes/wp-fits/sidebar.php
+++ wordpress/wp-content/themes/wp-fits/sidebar.php
@@ -4,27 +4,4 @@
   <?php else : ?>
   <?php endif; ?>
 
-  <?php if (  is_page(22) )  { ?>
-    <div class="widget widget-last-news">
-      <span class="widget-title">Последние статьи</span>
-
-      <?php query_posts("showposts=5"); ?>
-        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-          <div class="last-news">
-           <a href="<?php the_permalink(); ?>">
-              <span class="category-block-img">
-                <?php if ( has_post_thumbnail()) :
-                  the_post_thumbnail('small');
-                else: ?>
-                  <img src="<?php echo catchFirstImage(); ?>" title="<?php the_title(); ?>" alt="<?php the_title(); ?>" />
-                <?php endif; ?>
-              </span>
-             <?php wpeExcerpt('wpeExcerpt6'); ?>
-           </a>
-         </div><!-- last-news -->
-       <?php endwhile; endif; ?>
-      <?php wp_reset_query(); ?>
-    </div><!-- /.widget widget-last-news -->
-  <?php } ?>
-
 </aside>
